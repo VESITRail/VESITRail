661d502c7081a51cc27aec75b5d2a9c19d313b38
Jay Kerkar <bytewise0405@gmail.com>
2025-05-29 23:24:48 +0530

    feat: add ThemeProvider and PWAInstallButton components with navigation items

---
+++ src/config/navigation.ts
@@ -0,0 +1,24 @@
+import { Home, Mail, Users, BookOpen } from "lucide-react";
+
+export const navigationItems = [
+  {
+    href: "/",
+    icon: Home,
+    label: "Home",
+  },
+  {
+    icon: BookOpen,
+    label: "Guide",
+    href: "/#guide",
+  },
+  {
+    icon: Users,
+    label: "Developers",
+    href: "/#developers",
+  },
+  {
+    icon: Mail,
+    label: "Contact",
+    href: "/#contact",
+  },
+];
