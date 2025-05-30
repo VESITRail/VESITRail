0b5dd3bf4d69d1473d9ddc4f4e61f9b4a084f944
Jay Kerkar <bytewise0405@gmail.com>
2025-05-30 13:42:27 +0530

    feat: add Badge and Card components with associated functionality fix: remove border from Heading2 component styling

--- src/components/ui/typography/heading-2.tsx
+++ src/components/ui/typography/heading-2.tsx
@@ -8,7 +8,7 @@ const Heading2 = ({ children, className, ...props }: Heading2Props) => {
   return (
     <h2
       className={cn(
-        "scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0",
+        "scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0",
         className
       )}
       {...props}
