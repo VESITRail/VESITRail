6b9f9c4d0da0c233be661d05d11983f8d0e50d6f
Jay Kerkar <bytewise0405@gmail.com>
2025-06-07 22:57:35 +0530

    feat: create index file for onboarding steps, exporting Review, Document, TravelInfo, AcademicInfo, and PersonalInfo components

---
+++ src/components/onboarding/steps/index.ts
@@ -0,0 +1,5 @@
+export { default as Review } from "./review";
+export { default as Document } from "./document";
+export { default as TravelInfo } from "./travel-info";
+export { default as AcademicInfo } from "./academic-info";
+export { default as PersonalInfo } from "./personal-info";
