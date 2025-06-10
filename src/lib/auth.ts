db11f5831effb408f8d669b27adf346e2f9eb45c
Jay Kerkar <bytewise0405@gmail.com>
2025-06-10 14:14:58 +0530

    Added middleware to check the email domain

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,6 @@
 import { betterAuth } from "better-auth";
+import { createAuthMiddleware, APIError } from "better-auth/api";
+
 import { nextCookies } from "better-auth/next-js";
 import { PrismaClient } from "@/generated/prisma";
 import { prismaAdapter } from "better-auth/adapters/prisma";
@@ -14,6 +16,24 @@ export const auth = betterAuth({
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+  databaseHooks: {
+    user: {
+      create: {
+        before: async (user) => {
+          const emailDomainCheck = /^[a-zA-Z0-9._%+-]+@ves\.ac\.in$/;
+
+          if (!user.email || !emailDomainCheck.test(user.email)) {
+            throw new APIError("BAD_REQUEST", {
+              code: "INVALID_EMAIL_DOMAIN",
+              message: "Only emails with @ves.ac.in domain are allowed",
+            });
+          }
+
+          return { data: { ...user } };
+        },
+      },
     },
   },
 });
