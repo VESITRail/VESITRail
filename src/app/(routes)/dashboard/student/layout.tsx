13492f36133de06eee9db0dd03f5307bea710b71
Jay Kerkar <bytewise0405@gmail.com>
2025-06-11 21:59:53 +0530

    refactor: update student layout component to use StudentDashboardLayoutContent and simplify structure

--- src/app/(routes)/dashboard/student/layout.tsx
+++ src/app/(routes)/dashboard/student/layout.tsx
@@ -1,52 +1,14 @@
-import {
-  SidebarInset,
-  SidebarTrigger,
-  SidebarProvider,
-} from "@/components/ui/sidebar";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
-import AppSidebar from "@/components/app-sidebar";
-import { Separator } from "@/components/ui/separator";
+import { Metadata } from "next";
+import StudentDashboardLayoutContent from "@/components/layout-content/student-dashboard-layout-content";
 
-const StudentLayout = () => {
+export const metadata: Metadata = {
+  title: "Student Dashboard | VESITRail",
+};
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-
-            <Separator
-              orientation="vertical"
-              className="mr-2 data-[orientation=vertical]:h-4"
-            />
-
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="#">
-                    Building Your Application
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator className="hidden md:block" />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
-          </div>
-        </header>
-      </SidebarInset>
-    </SidebarProvider>
+    <StudentDashboardLayoutContent>{children}</StudentDashboardLayoutContent>
   );
 };
 
-export default StudentLayout;
+export default DashboardLayout;
