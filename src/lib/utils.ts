1d3df9f7dae4748cabe4b17cf85eb771f3537810
Jay Kerkar <bytewise0405@gmail.com>
2025-06-13 16:33:38 +0530

    fix: update toTitleCase function to handle multiple delimiters and ensure proper casing

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -13,8 +13,9 @@ const toTitleCase = (str?: string) => {
   if (!str) return "";
 
   return str
-    .split("-")
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .toLowerCase()
+    .split(/[\s-]+/)
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
     .join(" ");
 };
 
