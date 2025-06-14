d17cf31d8f5fa74dc8c68c7578b0a850d440c353
Jay Kerkar <bytewise0405@gmail.com>
2025-06-14 21:24:53 +0530

    fix: add key prop to SidebarGroup in NavMain component to ensure unique identification of elements

--- src/components/nav-main.tsx
+++ src/components/nav-main.tsx
@@ -16,11 +16,14 @@ const NavMain = ({
     items: { url: string; name: string; icon: LucideIcon }[];
   }[];
 }) => {
-  return navMain.map((element) => {
+  return navMain.map((element, index) => {
     const { label, items } = element;
 
     return (
-      <SidebarGroup className="group-data-[collapsible=icon]:hidden">
+      <SidebarGroup
+        key={index}
+        className="group-data-[collapsible=icon]:hidden"
+      >
         <SidebarGroupLabel>{label}</SidebarGroupLabel>
         <SidebarMenu>
           {items.map((item) => (
