d6becf5a4cf609a766f3c4dbc9020cd508bffed2
Jay Kerkar <bytewise0405@gmail.com>
2025-06-16 14:21:50 +0530

    refactor: simplify getUserInitials usage and update Avatar component for consistent styling

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -12,11 +12,11 @@ import {
   DropdownMenuContent,
   DropdownMenuSeparator,
 } from "@/components/ui/dropdown-menu";
-import { toTitleCase } from "@/lib/utils";
 import { useRouter } from "next/navigation";
 import { authClient } from "@/lib/auth-client";
 import { Skeleton } from "@/components/ui/skeleton";
 import { LogOut, ChevronsUpDown } from "lucide-react";
+import { getUserInitials, toTitleCase } from "@/lib/utils";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const NavUser = () => {
@@ -24,24 +24,13 @@ const NavUser = () => {
   const { isMobile } = useSidebar();
   const { data, isPending } = authClient.useSession();
 
-  const getUserInitials = (name?: string) => {
-    if (!name) return "ST";
-
-    return name
-      .split(" ")
-      .map((n) => n[0])
-      .join("")
-      .toUpperCase()
-      .slice(0, 2);
-  };
-
   if (isPending) {
     return (
       <SidebarMenu>
         <SidebarMenuItem>
           <SidebarMenuButton size="lg" className="opacity-50">
             <div className="flex items-center gap-2">
-              <Skeleton className="h-8 w-8 rounded-lg" />
+              <Skeleton className="size-8 rounded-lg" />
 
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <Skeleton className="h-4 w-24" />
@@ -63,13 +52,13 @@ const NavUser = () => {
               size="lg"
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground cursor-pointer"
             >
-              <Avatar className="h-8 w-8 rounded-lg">
+              <Avatar className="size-8 rounded-lg">
                 <AvatarImage
                   alt={data?.user.name || "Student"}
                   src={data?.user.image || undefined}
                 />
                 <AvatarFallback className="rounded-lg">
-                  {getUserInitials(data?.user.name)}
+                  {getUserInitials("Student", data?.user.name)}
                 </AvatarFallback>
               </Avatar>
 
@@ -98,7 +87,7 @@ const NavUser = () => {
                     src={data?.user.image || undefined}
                   />
                   <AvatarFallback className="rounded-lg">
-                    {getUserInitials(data?.user.name)}
+                    {getUserInitials("Student", data?.user.name)}
                   </AvatarFallback>
                 </Avatar>
 
