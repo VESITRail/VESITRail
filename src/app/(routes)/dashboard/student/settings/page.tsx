7dd673d8b1e6b8ca3db102b9a87d0ab42682244c
Jay Kerkar <bytewise0405@gmail.com>
2025-06-17 16:53:06 +0530

    feat: create Settings component for managing student settings with preferences and layout

---
+++ src/app/(routes)/dashboard/student/settings/page.tsx
@@ -0,0 +1,25 @@
+import { Separator } from "@/components/ui/separator";
+import { Settings as SettingsIcon } from "lucide-react";
+import Preferences from "@/components/settings/preferences";
+
+const Settings = () => {
+  return (
+    <div className="container max-w-5xl mx-auto py-12 px-4">
+      <div className="flex justify-between items-center">
+        <span className="flex items-center gap-3">
+          <div className="size-10 bg-primary/20 rounded-lg flex items-center justify-center">
+            <SettingsIcon className="size-5" />
+          </div>
+
+          <h1 className="text-2xl font-semibold">Manage Settings</h1>
+        </span>
+      </div>
+
+      <Separator className="my-6" />
+
+      <Preferences />
+    </div>
+  );
+};
+
+export default Settings;
