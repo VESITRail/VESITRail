b20018030c4eaafdc443533510b964af6683c9bd
Jay Kerkar <bytewise0405@gmail.com>
2025-09-09 15:54:05 +0530

    Added meta title to the /dasboard route

--- src/app/(routes)/dashboard/page.tsx
+++ src/app/(routes)/dashboard/page.tsx
@@ -1,5 +1,10 @@
+import { Metadata } from "next";
 import { Loader2 } from "lucide-react";
 import Status from "@/components/ui/status";
+
+export const metadata: Metadata = {
+  title: "Dashboard | VESITRail",
+};
 
 const Dashboard = () => {
   return (
