8d76d5a7b30e8166df755ac791aef244c1e7f801
Jay Kerkar <bytewise0405@gmail.com>
2025-09-14 16:46:09 +0530

    feat: add rewrites for ingest paths to PostHog assets

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,19 @@ const withSerwist = withSerwistInit({
 });
 
 const nextConfig: NextConfig = {
+  skipTrailingSlashRedirect: true,
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://eu-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://eu.i.posthog.com/:path*",
+      },
+    ];
+  },
   webpack: (config, { isServer }) => {
     if (isServer) {
       const {
