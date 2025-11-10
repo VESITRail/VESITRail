310268c8e31bbc94cc6f13c597fa04370037558a
Jay Kerkar <bytewise0405@gmail.com>
2025-11-10 21:52:35 +0530

    chore: reorder Playwright configuration for clarity

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,15 +1,16 @@
 import { devices, defineConfig } from "@playwright/test";
 
 export default defineConfig({
-	use: {
-		trace: "on-first-retry"
-	},
 	reporter: "html",
 	testDir: "./tests",
 	fullyParallel: true,
 	forbidOnly: !!process.env.CI,
 	retries: process.env.CI ? 2 : 0,
 	workers: process.env.CI ? 1 : undefined,
+	use: {
+		trace: "on-first-retry",
+		baseURL: "http://localhost:3000"
+	},
 	webServer: {
 		port: 3000,
 		timeout: 120000,
