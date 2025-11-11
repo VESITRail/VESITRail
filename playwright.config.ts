2d76cf0b90356ef3b4511aeb6344d18a1bde605a
Jay Kerkar <bytewise0405@gmail.com>
2025-11-11 12:53:23 +0530

    chore: adjust Playwright configuration for timeout and locale settings

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,7 @@
 import { devices, defineConfig } from "@playwright/test";
 
 export default defineConfig({
+	timeout: 60000,
 	reporter: "html",
 	testDir: "./tests",
 	fullyParallel: true,
@@ -8,12 +9,13 @@ export default defineConfig({
 	retries: process.env.CI ? 2 : 0,
 	workers: process.env.CI ? 1 : undefined,
 	use: {
+		locale: "en-US",
 		trace: "on-first-retry",
+		video: "retain-on-failure",
 		baseURL: "http://localhost:3000"
 	},
 	webServer: {
 		port: 3000,
-		timeout: 300000,
 		reuseExistingServer: !process.env.CI,
 		command: "npm run build && npm run start"
 	},
