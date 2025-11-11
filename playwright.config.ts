2431d29afa36270c20a93f063ea9806f1dab9b44
Jay Kerkar <bytewise0405@gmail.com>
2025-11-11 13:06:05 +0530

    chore: update Playwright configuration with enhanced timeout settings

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,14 +8,20 @@ export default defineConfig({
 	forbidOnly: !!process.env.CI,
 	retries: process.env.CI ? 2 : 0,
 	workers: process.env.CI ? 1 : undefined,
+	expect: {
+		timeout: 10000
+	},
 	use: {
 		locale: "en-US",
+		actionTimeout: 15000,
 		trace: "on-first-retry",
+		navigationTimeout: 30000,
 		video: "retain-on-failure",
 		baseURL: "http://localhost:3000"
 	},
 	webServer: {
 		port: 3000,
+		timeout: 180000,
 		reuseExistingServer: !process.env.CI,
 		command: "npm run build && npm run start"
 	},
