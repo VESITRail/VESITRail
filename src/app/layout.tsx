75e82958da2bc90d4d4f2c7dea9dc12e62ef4cb8
Jay Kerkar <bytewise0405@gmail.com>
2026-08-19 19:18:39 +0530

    fix(metadata): update site metadata and icons for improved clarity and branding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,21 @@ const inter = Inter({
 export const metadata: Metadata = {
 	title: "VESITRail",
 	manifest: "/manifest.webmanifest",
+	description:
+		"Apply for railway student concessions online with ease! Enjoy a smooth application process and real-time tracking - Made for VESIT students.",
+	icons: {
+		icon: [
+			{ url: "/favicon.ico", sizes: "any" },
+			{ url: "/icon.svg", type: "image/svg+xml" },
+			{ url: "/icons/ios/16.png", sizes: "16x16", type: "image/png" },
+			{ url: "/icons/ios/32.png", sizes: "32x32", type: "image/png" },
+			{ url: "/icons/android/android-launchericon-48-48.png", sizes: "48x48", type: "image/png" },
+			{ url: "/icons/android/android-launchericon-96-96.png", sizes: "96x96", type: "image/png" },
+			{ url: "/icons/android/android-launchericon-192-192.png", sizes: "192x192", type: "image/png" }
+		],
+		shortcut: ["/favicon.ico"],
+		apple: [{ url: "/icons/ios/180.png", sizes: "180x180", type: "image/png" }]
+	},
 	verification: {
 		google: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION
 	},
@@ -22,9 +37,7 @@ export const metadata: Metadata = {
 		{ name: "Afnan Kazi", url: "https://github.com/Afnankazi" },
 		{ name: "Jay Kerkar", url: "https://github.com/jaykerkar0405" }
 	],
-	metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL || "https://vesitrail.ves.ac.in"),
-	description:
-		"Apply for railway student concessions online with ease! Enjoy a smooth application process and real-time tracking - Made for VESIT students."
+	metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL || "https://rail.vesit.ves.ac.in")
 };
 
 const RootLayout = ({
